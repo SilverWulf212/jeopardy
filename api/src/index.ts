070b970b223dd1a2ddfb8c9268769c8691d7adbf
silverwulf212 <silverwulf@silverwulf.com>
2026-09-10 19:32:28 -0500

    Hide x-powered-by header

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,6 +9,7 @@ import { pool, query } from "./db.js";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const app = express();
+app.disable("x-powered-by");
 app.use(cors());
 app.use(express.json({ limit: "1mb" }));
 
